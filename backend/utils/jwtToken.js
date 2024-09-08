9c0963fc52f30ec27a2a54459ca336f0aac4b656
Aniket Savita <138676031+Aniket-8719@users.noreply.github.com>
2024-09-09 01:57:11 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -10,7 +10,7 @@ const sendToken = (user, statusCode, res)=>{
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         secure: true,
-        httpOnly:false,
+        httpOnly:true,
         sameSite: 'Lax',  // Required for cross-origin cookies
     };
 
